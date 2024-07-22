7f5e484f7e8f09a8d94c1d5c65d26058949b5fb1
chananyag <44477g@mail.com>
2024-07-22 11:34:49 +0300

    currnt base_socket

--- socket/Socket.h
+++ socket/Socket.h
@@ -1,39 +1,51 @@
-#ifndef SOCKET_HPP
-#define SOCKET_HPP
+// Definition of the Socket class
 
-#ifdef _WIN32
-    #include <winsock2.h>
-    #include <ws2tcpip.h>
-    #pragma comment(lib, "ws2_32.lib")
-    typedef int socklen_t;
-#else
-    #include <sys/types.h>
-    #include <sys/socket.h>
-    #include <netinet/in.h>
-    #include <arpa/inet.h>
-    #include <unistd.h>
-    #include <cstring>
-    #define INVALID_SOCKET -1
-    #define SOCKET_ERROR -1
-    typedef int SOCKET;
-#endif
+#pragma once
 
-#include <iostream>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
 #include <string>
+#include <arpa/inet.h>
 
-class Socket {
-public:
-    Socket();
-    ~Socket();
-    bool connect(const std::string& host, int port);
-    void close();
-    int send(const std::string& data);
-    int receive(char* buffer, int size);
 
-private:
-    SOCKET sock;
-    bool initialize();
-    void cleanup();
+const int MAXHOSTNAME = 200;
+const int MAXCONNECTIONS = 5;
+const int MAXRECV = 500;
+
+class Socket
+{
+ public:
+  Socket();
+  virtual ~Socket();
+
+  // Server initialization
+  bool create();
+  bool bind ( const int port );
+  bool listen() const;
+  bool accept ( Socket& ) const;
+
+  // Client initialization
+  bool connect ( const std::string host, const int port );
+
+  // Data Transimission
+  bool send ( const std::string ) const;
+  int recv ( std::string& ) const;
+
+
+  void set_non_blocking ( const bool );
+
+  bool is_valid() const { return m_sock != -1; }
+
+ private:
+
+  int m_sock;
+  sockaddr_in m_addr;
+
+
 };
 
-#endif // SOCKET_HPP
+
