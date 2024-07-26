c3fdbc9d2ce9bd4f995c556647cad6bb70b6a85f
itzhak pintel (mefathimtech19-Latitude-7490) <izpintel@gmail.com>
2024-07-26 19:04:33 +0300

    adding test cases for Aes string encryption

--- aes/test.cpp
+++ aes/test.cpp
@@ -33,3 +33,41 @@ TEST(AesTest, Aes_128_encript_decript_block) {
   aes.decript(decripted_block);
   EXPECT_EQ(decripted_block, expected_decripted_block);
 }
+
+TEST(AesTest, Aes_128_encript_decript_string_with_size_16) {
+  std::array<uint8_t, 16> key = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab,
+   0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+  std::string msg = "hello hello hsm!";
+  std::string expected_encripted_msg = "\xc2\x9e\x26\xc2\x8e\xdc\x25\x33\x44\x28\xe6\xa3\xe2\x72\x2d\xd2\xa2\x54\xbe\x88\xe0\x37\xdd\xd9\xd7\x9f\xb6\x41\x1c\x3f\x9d\xf8";
+  Aes<AesVariant::Aes128> aes(key);
+  EXPECT_EQ(aes.encript(msg), expected_encripted_msg);
+  EXPECT_EQ(aes.decript(expected_encripted_msg), msg);
+}
+
+TEST(AesTest, Aes_128_encript_decript_string_with_size_15) {
+  std::array<uint8_t, 16> key = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab,
+   0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+  std::string msg = "hello hello hsm";
+  std::string expected_encripted_msg = "\xbb\x6e\xd0\xed\x30\xfa\x44\x64\xd2\x1f\xf1\xcb\xd9\x27\xf9\x56";
+  Aes<AesVariant::Aes128> aes(key);
+  auto result = aes.encript(msg);
+
+  EXPECT_EQ(result.size(), expected_encripted_msg.size());
+
+  EXPECT_EQ(result, expected_encripted_msg);
+  EXPECT_EQ(aes.decript(expected_encripted_msg), msg);
+}
+
+TEST(AesTest, Aes_128_encript_decript_empty_string) {
+  std::array<uint8_t, 16> key = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab,
+   0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+  std::string msg = "";
+  std::string expected_encripted_msg = "\xa2\x54\xbe\x88\xe0\x37\xdd\xd9\xd7\x9f\xb6\x41\x1c\x3f\x9d\xf8";
+  Aes<AesVariant::Aes128> aes(key);
+  auto result = aes.encript(msg);
+
+  EXPECT_EQ(result.size(), expected_encripted_msg.size());
+
+  EXPECT_EQ(result, expected_encripted_msg);
+  EXPECT_EQ(aes.decript(expected_encripted_msg), msg);
+}
