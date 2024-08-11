6f50be391dfd6accaaf0a4c1e6f54306dd792696
Refael Brand <rafybrand@gmail.com>
2024-08-11 15:27:32 +0300

    Made ConditonsFactory more a generic class

--- MainComputer/include/ConditionsFactory.h
+++ MainComputer/include/ConditionsFactory.h
@@ -4,6 +4,12 @@
 #include <memory>
 
 #include "ConditionBase.h"
+#include "GreaterThanCondition.h"
+#include "SmallerThanCondition.h"
+#include "EqualsToCondition.h"
+#include "StartsWithCondition.h"
+#include "EndsWithCondition.h"
+#include "ContainsCondition.h"
 
 
 class ConditionsFactory
@@ -12,5 +18,24 @@ public:
 	std::shared_ptr<ConditionBase> createCondition(std::string input, std::string conditionType, std::string validationValue);
 
 private:
-
+    const std::unordered_map<std::string, std::function<std::shared_ptr<ConditionBase>(const std::string&, const std::string&)>> _conditionsMap = {
+		{"greater than", [](std::string input, std::string validationValue) {
+			return std::make_shared<GreaterThanCondition>(input, validationValue);
+		}},
+		{"smaller than", [](std::string input, std::string validationValue) {
+			return std::make_shared<SmallerThanCondition>(input, validationValue);
+		}},
+		{"equals to", [](std::string input, std::string validationValue) {
+			return std::make_shared<EqualsToCondition>(input, validationValue);
+		}},
+		{"starts with", [](std::string input, std::string validationValue) {
+			return std::make_shared<StartsWithCondition>(input, validationValue);
+		}},
+		{"ends with", [](std::string input, std::string validationValue) {
+			return std::make_shared<EndsWithCondition>(input, validationValue);
+		}},
+		{"contains", [](std::string input, std::string validationValue) {
+			return std::make_shared<ContainsCondition>(input, validationValue);
+		}}
+    };
 };
