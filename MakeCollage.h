75cf8085df8bd7e1e4bbe63259c3a385f2045b2f
davidmaroko <76688717+davidmaroko@users.noreply.github.com>
2024-08-14 17:41:35 +0300

    Update MakeCollage.h

--- MakeCollage.h
+++ MakeCollage.h
@@ -15,6 +15,13 @@ struct Box
     Box(const cv::Rect& original) : rect(original), originalRect(original) {}
 };
 
+std::deque<std::shared_ptr<Box>> getBoxesDQ(const std::vector<cv::Rect>& diff_boxes);
+void resizeBoxToLimit(const std::shared_ptr<Box>& box, int limit);
+std::vector<cv::Rect> getDetectedBoxes(cv::Mat& collage);
+void resizeOverLimitBoxes(std::deque<std::shared_ptr<Box>>& boxes);
+void drawFrameOnDetectedBox(const std::shared_ptr<Box>& box, const cv::Rect& detectedBox, cv::Mat& originalMat);
+void upgradeSizesToYolos(const std::shared_ptr<Box>& box, const cv::Rect& detectedBox);
+
 bool isOverlapSignificant(const cv::Rect& rect1, const cv::Rect& rect2, double threshold = 0.8);
 cv::Mat optimizeCollageGreedy(cv::Mat& originalMat, std::vector<cv::Rect>& diff_boxes);
 std::vector<std::shared_ptr<Box>> arrangeRectanglesInSquare_FFT(std::deque<std::shared_ptr<Box>>& boxes, int width, int height);
@@ -22,7 +29,8 @@ cv::Mat createCollage(cv::Mat& originalMat, std::vector<std::shared_ptr<Box>>& r
 void reverseCollage(cv::Mat& originalMat,
     const cv::Mat& collageMat,
     std::vector<std::shared_ptr<Box>>& results,
-    std::vector<cv::Rect>& detectedBoxes
+    std::vector<cv::Rect>& detectedBoxes,
+    cv::Mat& copyMat
 );
 
 #endif // MAKE_COLLAGE_H
