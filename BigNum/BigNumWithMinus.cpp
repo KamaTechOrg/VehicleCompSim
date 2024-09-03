758f1bb1e341f1ef425bc75df6384fb3bd4fef0d
ym987 <ymark987@gmail.com>
2024-09-03 12:47:37 +0300

    change for loop

--- BigNum/BigNumWithMinus.cpp
+++ BigNum/BigNumWithMinus.cpp
@@ -1,159 +1,196 @@
 #include "BigNumWithMinus.h"
 
-
-
-    void BigNumWithMinus::setToMinus() {
-        for (auto& i : data) {
-            i = ~i;
-        }
-        *this += 1;
+void BigNumWithMinus::setToMinus()
+{
+    for (int i = 0; i < size; ++i)
+    {
+        data[i] = ~data[i];
     }
+    *this += 1;
+}
 
-    void BigNumWithMinus::setToPlus() {
-        for (auto& i : data) {
-            i = ~i;
-        }
-        *this += 1;
+void BigNumWithMinus::setToPlus()
+{
+    for (int i = 0; i < size; ++i)
+    {
+        data[i] = ~data[i];
     }
+    *this += 1;
+}
 
-    bool BigNumWithMinus::operator<(const BigNumWithMinus& other) const {
-        if (this->isMinus() && !other.isMinus()) {
-            return true;
-        }
-        if (!this->isMinus() && other.isMinus()) {
-            return false;
-        }
-        return BigNum::operator<(other);
+bool BigNumWithMinus::operator<(const BigNumWithMinus &other) const
+{
+    if (this->isMinus() && !other.isMinus())
+    {
+        return true;
     }
-
-    bool BigNumWithMinus::operator<(uint32_t num) const {
-        return *this < BigNumWithMinus(num, MAX_SIZE * UINT_T_SIZE);
+    if (!this->isMinus() && other.isMinus())
+    {
+        return false;
     }
+    return BigNum::operator<(other);
+}
 
-    bool BigNumWithMinus::operator>(const BigNumWithMinus& other) const {
-        return !(*this < other) && !(*this == other);
+bool BigNumWithMinus::operator<(uint32_t num) const
+{
+    return *this < BigNumWithMinus(num, MAX_SIZE * UINT_T_SIZE);
+}
+
+bool BigNumWithMinus::operator>(const BigNumWithMinus &other) const
+{
+    return !(*this < other) && !(*this == other);
+}
+
+bool BigNumWithMinus::operator>(uint32_t num) const
+{
+    return *this > BigNumWithMinus(num, MAX_SIZE * UINT_T_SIZE);
+}
+
+bool BigNumWithMinus::operator<=(const BigNumWithMinus &other) const
+{
+    return !(*this > other);
+}
+
+bool BigNumWithMinus::operator<=(uint32_t num) const
+{
+    return *this <= BigNumWithMinus(num, MAX_SIZE * UINT_T_SIZE);
+}
+
+bool BigNumWithMinus::operator>=(const BigNumWithMinus &other) const
+{
+    return !(*this < other);
+}
+
+bool BigNumWithMinus::operator>=(uint32_t num) const
+{
+    return *this >= BigNumWithMinus(num, MAX_SIZE * UINT_T_SIZE);
+}
+
+BigNumWithMinus BigNumWithMinus::operator-(const BigNumWithMinus &other) const
+{
+    BigNumWithMinus tmp = other;
+    tmp.setToMinus();
+    return *this + tmp;
+}
+
+BigNumWithMinus &BigNumWithMinus::operator-=(const BigNumWithMinus &other)
+{
+    *this = *this - other;
+    return *this;
+}
+
+BigNumWithMinus BigNumWithMinus::operator*(const BigNumWithMinus &other) const
+{
+    BigNumWithMinus tmpOther = other;
+    if (tmpOther.isMinus())
+    {
+        tmpOther.setToPlus();
     }
-
-    bool BigNumWithMinus::operator>(uint32_t num) const {
-        return *this > BigNumWithMinus(num, MAX_SIZE * UINT_T_SIZE);
+    BigNumWithMinus toReturn = *this;
+    if (toReturn.isMinus())
+    {
+        toReturn.setToPlus();
     }
+    toReturn.BigNum::operator*=(tmpOther);
 
-    bool BigNumWithMinus::operator<=(const BigNumWithMinus& other) const {
-        return !(*this > other);
+    if (this->isMinus() ^ other.isMinus())
+    {
+        toReturn.setToMinus();
     }
+    return toReturn;
+}
 
-    bool BigNumWithMinus::operator<=(uint32_t num) const {
-        return *this <= BigNumWithMinus(num, MAX_SIZE * UINT_T_SIZE);
+BigNumWithMinus &BigNumWithMinus::operator*=(const BigNumWithMinus &other)
+{
+    *this = *this * other;
+    return *this;
+}
+
+BigNumWithMinus BigNumWithMinus::operator/(const BigNumWithMinus &other) const
+{
+    BigNumWithMinus tmpOther = other;
+    if (tmpOther.isMinus())
+    {
+        tmpOther.setToPlus();
     }
-
-    bool BigNumWithMinus::operator>=(const BigNumWithMinus& other) const {
-        return !(*this < other);
+    BigNumWithMinus toReturn = *this;
+    if (toReturn.isMinus())
+    {
+        toReturn.setToPlus();
     }
-
-    bool BigNumWithMinus::operator>=(uint32_t num) const {
-        return *this >= BigNumWithMinus(num, MAX_SIZE * UINT_T_SIZE);
+    toReturn.BigNum::operator/=(tmpOther);
+    if (this->isMinus() ^ other.isMinus())
+    {
+        toReturn.setToMinus();
     }
+    return toReturn;
+}
 
-    BigNumWithMinus BigNumWithMinus::operator-(const BigNumWithMinus& other) const {
-        BigNumWithMinus tmp = other;
-        tmp.setToMinus();
-        return *this + tmp;
+BigNumWithMinus BigNumWithMinus::operator/(uint32_t num) const
+{
+    return *this / BigNumWithMinus(num, 32);
+}
+
+BigNumWithMinus &BigNumWithMinus::operator/=(const BigNumWithMinus &other)
+{
+    *this = *this / other;
+    return *this;
+}
+
+BigNumWithMinus BigNumWithMinus::operator%(const BigNumWithMinus &other) const
+{
+    BigNumWithMinus tmpOther = other;
+    if (tmpOther.isMinus())
+    {
+        tmpOther.setToPlus();
     }
-
-    BigNumWithMinus& BigNumWithMinus::operator-=(const BigNumWithMinus& other) {
-        *this = *this - other;
-        return *this;
+    BigNumWithMinus toReturn = *this;
+    if (toReturn.isMinus())
+    {
+        toReturn.setToPlus();
     }
-
-    BigNumWithMinus BigNumWithMinus::operator*(const BigNumWithMinus& other) const {
-        BigNumWithMinus tmpOther = other;
-        if (tmpOther.isMinus()) {
-            tmpOther.setToPlus();
-        }
-        BigNumWithMinus toReturn = *this;
-        if (toReturn.isMinus()) {
+    toReturn = toReturn.BigNum::operator%(tmpOther);
+    if (this->isMinus() && other.isMinus())
+    {
+        toReturn.setToMinus();
+    }
+    else if (this->isMinus() ^ other.isMinus())
+    {
+        toReturn -= tmpOther;
+        if (this->isMinus())
+        {
             toReturn.setToPlus();
         }
-        toReturn.BigNum::operator*=(tmpOther);
-
-        if (this->isMinus() ^ other.isMinus()) {
-            toReturn.setToMinus();
-        }
-        return toReturn;
     }
 
-    BigNumWithMinus& BigNumWithMinus::operator*=(const BigNumWithMinus& other) {
-        *this = *this * other;
-        return *this;
-    }
+    return toReturn;
+}
 
-    BigNumWithMinus BigNumWithMinus::operator/(const BigNumWithMinus& other) const {
-        BigNumWithMinus tmpOther = other;
-        if (tmpOther.isMinus()) {
-            tmpOther.setToPlus();
-        }
-        BigNumWithMinus toReturn = *this;
-        if (toReturn.isMinus()) {
-            toReturn.setToPlus();
-        }
-        toReturn.BigNum::operator/=(tmpOther);
-        if (this->isMinus() ^ other.isMinus()) {
-            toReturn.setToMinus();
-        }
-        return toReturn;
-    }
+BigNumWithMinus BigNumWithMinus::operator%(uint32_t num) const
+{
+    return *this % BigNumWithMinus(num, 32);
+}
 
-    BigNumWithMinus BigNumWithMinus::operator/(uint32_t num) const {
-        return *this / BigNumWithMinus(num, 32);
-    }
+bool BigNumWithMinus::isMinus() const
+{
+    return data[size - 1] & (1 << (UINT_T_SIZE - 1));
+}
 
-    BigNumWithMinus& BigNumWithMinus::operator/=(const BigNumWithMinus& other) {
-        *this = *this / other;
-        return *this;
-    }
+void BigNumWithMinus::print() const
+{
+    std::cout << toString() << std::endl;
+}
 
-    BigNumWithMinus BigNumWithMinus::operator%(const BigNumWithMinus& other) const {
-        BigNumWithMinus tmpOther = other;
-        if (tmpOther.isMinus()) {
-            tmpOther.setToPlus();
-        }
-        BigNumWithMinus toReturn = *this;
-        if (toReturn.isMinus()) {
-            toReturn.setToPlus();
-        }
-        toReturn = toReturn.BigNum::operator%(tmpOther);
-        if (this->isMinus() && other.isMinus()) {
-            toReturn.setToMinus();
-        }
-        else if(this->isMinus() ^ other.isMinus()) {
-            toReturn -= tmpOther;
-            if(this->isMinus()) {
-                toReturn.setToPlus();            
-            }
-        }
-        
-        return toReturn;
+std::string BigNumWithMinus::toString() const
+{
+    if (!isMinus())
+    {
+        return BigNum::toString();
     }
-
-    BigNumWithMinus BigNumWithMinus::operator%(uint32_t num) const {
-        return *this % BigNumWithMinus(num, 32);
+    else
+    {
+        BigNumWithMinus tmp = *this;
+        tmp.setToPlus();
+        return "-" + tmp.BigNum::toString();
     }
-
-    bool BigNumWithMinus::isMinus() const {
-        return data[size - 1] & (1 << (UINT_T_SIZE - 1));
-    }
-
-    void BigNumWithMinus::print() const {
-        std::cout << toString() << std::endl;
-    }
-
-    std::string BigNumWithMinus::toString() const {
-        if (!isMinus()) {
-            return BigNum::toString();
-        } else {
-            BigNumWithMinus tmp = *this;
-            tmp.setToPlus();
-            return "-" + tmp.BigNum::toString();
-        }
-    }
-
+}
