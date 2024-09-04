02d87ca5352dcdfb641c116565495e0317d6aa0e
itzhaksh <itzhakshif@gmail.com>
2024-09-04 15:42:41 +0300

    Added Action Management and Message Parsing to ConditionsManager

--- MainComputer/include/conditionsManager.h
+++ MainComputer/include/conditionsManager.h
@@ -1,21 +1,27 @@
 #ifndef CONDITIONS_MANAGER_H
 #define CONDITIONS_MANAGER_H
 
+#include "ConditionBase.h"
+#include "Communication.h"
+#include "Action.h" 
+
 #include <vector>
 #include <string>
 #include <memory>
-
+#include <unordered_map> 
 #include "json.hpp"
-#include "ConditionBase.h"
-#include "Communication.h"
+
 
 class ConditionsManager {
 private:
     static std::vector<std::shared_ptr<ConditionBase>> conditions;
-    // static becuase we want the save button from the UI to be able to reload the conditions from the saved JSON file
+    static std::unordered_map<std::string, Action> actions; 
 
     void addCondition(std::shared_ptr<ConditionBase> condition);
-    bool validateAll(const std::string& senderId, const std::string& value) const;
+    void addAction(const std::string &id, const Action &action);     
+    bool validateAll(const std::string &senderId, const std::string &value) const;
+    void executeAction(const std::string &id);  // Execute the action associated with the ID
+
     bool _isRunning;
 
 public:
@@ -23,7 +29,8 @@ public:
     void run();
     void stop();
     bool isRunning();
-    void loadFromJson(const std::string& filename);
+    void loadFromJson(const std::string &filename);
+    std::pair<std::string, std::string> parseMessage(const std::string &message);  // Parse the message to extract ID and value
 };
 
 #endif
