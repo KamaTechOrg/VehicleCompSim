5308166beb6ee062082616874307d835e9744772
chananyag <44477g@mail.com>
2024-09-08 15:53:01 +0300

    test first

---
+++ tests/test_client.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <gtest/gtest.h>
+#include "client.h"
+#include "manger.h"
+
+
+TEST(ClientSocketTest, IsValidPointer) {
+    
+    ClientSocket socket(11);
+    // ASSERT_EQ(1 , socket.is_valid_size(10));
+
+}
