462856d75267d0e34e4884705cf3368054647544
ym987 <ymark987@gmail.com>
2024-09-11 15:52:07 +0300

    basic API

---
+++ HSM/include/HSM.h
@@ -0,0 +1,131 @@
+#pragma once
+
+#include <string>
+#include <utility>
+
+
+class HSM
+{
+public:
+    std::pair<std::string, std::string> generateAsymmetricKeys(std::string type, int bits);
+
+    std::string generateSymmetricKey(std::string type);
+
+
+    std::string encrypt(std::string message, std::string type, std::string key);
+
+    std::string decrypt(std::string message, std::string type, std::string key);
+
+    std::string sign(std::string message, std::string type, std::string key);
+
+    std::string verify(std::string message, std::string signature, std::string type, std::string key);
+
+
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+egarding interfaces - we will need the low-level interfaces anyway, in order to do the job internally. If we decide to add something on top of this as more high-level, we can always do this afterwards, and hide the specific keys definitions and work with predefined configurations only
+
+I started to define the interfaces (thiugh this better should be done with students as it is essentially the most important part the I can contribute), and realized that it's more proper to do it with templates. Should we use templates, or only old C++ inheritance, or maybe plain C API?
+
+below is some approximate API in C++ without templates
+
+So, let's assume we have a notation of crypto key
+class CryptoKey {
+};
+
+and derivative 
+class EncryptionKey : public CryptoKey {};
+class DecryptionKey : public CryptoKey {};
+
+and also
+class SigningKey : public CryptoKey {};
+class VerificationKey : public CryptoKey {};
+
+apparently, some types of keys will inherit from more that one, so they will have to handle the virtual inheritance in this case
+
+Now, you have to define the algorithm. For complete fun, let's define a a base class for the algorithm
+
+class CryptoAlgorithm {};
+and defivatives
+class EncryptionAlgorithm : public CryptoAlgorithm {};
+class SigningAlgorithm : public CryptoAlgorithm {};
+class SymmetricEncryptionAlgorithm : public EnryptionAlgorithm
+class AES: public SymmetricEncryptionAlgorithm 
+class AES128: public AES
+
+we will also need the same for hashes. Since all hashes have the same interface, this shoul be easy
+
+class CryptoHash {
+public:
+      virtual uint32_t append(const std::ByteArray & data) = 0;
+               virtual uint32_t finalize() = 0;
+               virtual uint32_t getResult(std::ByteArray & result) = 0;
+};
+
+so that particular hashes will look like
+class SHA256 : public CryptoHash {};
+
+you can define the streams also as classes
+class ChaningStream{
+public:
+  ChaningStream(SymmetricEncryptionAlgorithm algo);
+  encrypt(const std::ByteArray & IV, const std::ByteArray & data, std::ByteArray & output) = 0;
+  decrypt(const std::ByteArray & IV, const std::ByteArray & data, std::ByteArray & output) = 0;
+}
+
+class CBC : public ChaningStream {}
+
+class AES_CBC : public CBC // THIS SHOULD BE DONE WITH TEMPLATES for generic solution
+
+and typical flow of encryption of data will be
+//create the cryptoKey
+AES128 enckey (random(keylen));
+AES128CBC encAlgo;
+std::ByteArray IV(random(IV_LEN));
+std::ByteArray output(data_len)
+encAlgo.encrypt(encKey, IV, data, output)
+
+*/
---
+++ HSM/src/HSM.cpp
@@ -0,0 +1,32 @@
+#include "HSM.h"
+
+
+std::pair<std::string, std::string> HSM::generateAsymmetricKeys(std::string type, int bits)
+{
+    return std::pair<std::string, std::string>();
+}
+
+std::string HSM::generateSymmetricKey(std::string type)
+{
+    return std::string();
+}
+
+std::string HSM::encrypt(std::string message, std::string type, std::string key)
+{
+    return std::string();
+}
+
+std::string HSM::decrypt(std::string message, std::string type, std::string key)
+{
+    return std::string();
+}
+
+std::string HSM::sign(std::string message, std::string type, std::string key)
+{
+    return std::string();
+}
+
+std::string HSM::verify(std::string message, std::string signature, std::string type, std::string key)
+{
+    return std::string();
+}
