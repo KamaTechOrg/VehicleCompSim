1383b219e732e9f6ddea6ca5abe169efcef2f681
34412as@gmail.com <34412as@gmail.com>
2024-09-15 14:35:10 +0300

    detect left and ritght departure

--- lane_departure.cpp
+++ lane_departure.cpp
@@ -2,12 +2,13 @@
 
 
 // Lane departure detection logic
-bool is_lane_departure(const cv::Mat& image, const std::vector<std::vector<int>>& lanes, int threshold) {
+// returns 1 for right departure, 2 for left departure, and 0 for not departure
+int is_lane_departure(const cv::Mat& image, const std::vector<std::vector<int>>& lanes, int threshold) {
     int frame_width = image.cols;
 
     if (lanes.size() != 2) {
         // Not enough lane lines detected to assess lane departure
-        return false;
+        return 0;
     }
 
     int left_x2 = lanes[0][2];  // x-coordinate of the right end of the left lane line
@@ -15,10 +16,10 @@ bool is_lane_departure(const cv::Mat& image, const std::vector<std::vector<int>>
     int lane_center = (left_x2 + right_x2) / 2;  // Center of the detected lane
     int frame_center = frame_width / 2;  // Center of the frame
 
-    //// Calculate smoothed lane center
-    //int smoothed_lane_center = get_smoothed_lane_center(lane_center);
-
-    //// Deviation threshold can now be passed as a parameter
-    //int deviation = smoothed_lane_center - frame_center;
-    return abs(lane_center - frame_center) > threshold;
+    if (lane_center - frame_center > threshold) {
+        return 1;
+    }
+    if (lane_center - frame_center < 0 - threshold)
+        return 2;
+    return 0;
 }
