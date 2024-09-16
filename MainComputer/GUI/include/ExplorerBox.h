76e6006403539a531fef552f860ac69c6823432a
Refael Brand <rafybrand@gmail.com>
2024-09-16 11:50:52 +0300

    Added scenarios list to Explorer box

--- MainComputer/GUI/include/ExplorerBox.h
+++ MainComputer/GUI/include/ExplorerBox.h
@@ -1,9 +1,15 @@
 #pragma once
 
 #include <QGroupBox>
+#include <QListView>
+#include <QVBoxLayout>
 
 class ExplorerBox : public QGroupBox
 {
 public:
 	ExplorerBox();
+
+private:
+	QVBoxLayout* _layout;
+	QListView* _scenariosList;
 };
--- MainComputer/GUI/src/ExplorerBox.cpp
+++ MainComputer/GUI/src/ExplorerBox.cpp
@@ -1,6 +1,21 @@
 #include "ExplorerBox.h"
 
+#include <QStringListModel>
+
 ExplorerBox::ExplorerBox()
 	: QGroupBox("Explorer")
 {
+	_scenariosList = new QListView;
+
+	QStringListModel* model = new QStringListModel(this);
+	QStringList scenarioList;
+	scenarioList << "scenario 1" << "scenario 2" << "scenario 3"
+		<< "scenario 4" << "scenario 5";
+
+	model->setStringList(scenarioList);
+	_scenariosList->setModel(model);
+
+	_layout = new QVBoxLayout;
+	_layout->addWidget(_scenariosList);
+	setLayout(_layout);
 }
