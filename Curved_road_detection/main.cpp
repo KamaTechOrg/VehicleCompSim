69089c64253cf083fed1fa85b0aa13ea1fcb33f1
34412as@gmail.com <34412as@gmail.com>
2024-09-18 16:30:03 +0300

    build bacik proj

---
+++ Curved_road_detection/main.cpp
@@ -0,0 +1,15 @@
+#ifdef _DEBUG
+// the debug version that corresponds to the opencv_world420d.dll:
+#pragma comment(lib, "opencv_world454d.lib")
+#else
+// the release version that corresponds to the opencv_world420.dll:
+#pragma comment(lib, "opencv_world454.lib")
+#endif
+
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+
+}
