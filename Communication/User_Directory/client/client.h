ea4bb8a7ef3acdf93bc6db624a963f73e3332651
dmaro <dmaroko21@gmai.com>
2024-09-22 13:51:26 +0300

    new client server

---
+++ Communication/User_Directory/client/client.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <future>
+
+#include "socket.h"
+#include "constants.h"
+#include "data_manipulator.h"
+
+
+class ClientSocket
+{
+
+public:
+    ClientSocket( int id);
+
+    sendErrorCode send(void *data, size_t size, int source_id, int dest_id);
+    std::pair<ListenErrorCode,int> listen(void *data, size_t size);
+    std::future<void> listenAsync(void *data, size_t size, std::function<void(ListenErrorCode)> callback);
+    void shut_down();
+
+private:
+    bool is_valid_ptr(void *ptr);
+    bool is_valid_size(size_t size);
+
+    Socket m_clientSocket;
+    int my_id;
+    
+};
---
+++ Communication/User_Directory/utilities/canbus.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+#include "data_manipulator.h"
+
+class CanBus {
+
+public:
+    
+    CanBus(int srcId, int dstId, std::string msg, int msgLen);
+    bool operator >(const CanBus& rhs) const ;
+    int getSourceId() const {return sourceId;}
+
+
+    int getDestinationId() const {
+        return destId;
+    }
+
+     int getSourcId() const {
+        return sourceId;
+    }
+
+    int getMessageSize() const {
+        return messageLength;
+    }
+
+    std::string getMessage() const {
+        return message;
+    }
+    std::string getFormattedMessage() const;
+
+    int crc;
+    
+    
+private:
+    int destId;
+    std::string message;
+    int messageLength;
+    int sourceId;
+  
+    bool rtr;
+    
+};
+
+void writeCanMessageToLog(CanBus & message , const std::string & filename);
