94dcee00634a236fdf2fd81f371d2af7d8bcf388
ym987 <ymark987@gmail.com>
2024-09-23 13:14:41 +0300

    add hsm status

--- HSM/RSA/include/RSA_KEY.h
+++ HSM/RSA/include/RSA_KEY.h
@@ -11,7 +11,7 @@
 #include <stdexcept>
 #include "../../BigNum/include/BigNum.h"
 #include "../../BigNum/include/BigNumWithMinus.h"
-#include "HSM_status_enum.cpp"
+#include "HSM_status_enum.hpp"
 
 
 class RSA_KEY
---
+++ HSM/Status/StatusEnum.hpp
@@ -0,0 +1,18 @@
+namespace HSM {
+  /**
+   * @enum HsmStatus
+   * @brief Status for the HSM module.
+   */
+  enum HsmStatus {
+    HSM_Good,
+    HSM_InvalidAlg,
+    HSM_InvalidIdent,
+    HSM_NoSuchKey,
+    HSM_KeyNotValid,
+    HSM_WrongDataSize,
+    HSM_BadOutputBufSize,
+    HSM_InternalErr,
+    // Add more here
+    HSM_Status_Count
+  };
+};
