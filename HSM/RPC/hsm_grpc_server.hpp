32656c9f9915b0c3c0996fc0ea5d8763c487f4f0
itzhak pintel (itzhak-vostro3520) <izpintel@gmail.com>
2024-10-14 22:52:55 +0300

    doxygen comments for RPC::HSMServer

--- HSM/RPC/hsm_grpc_server.hpp
+++ HSM/RPC/hsm_grpc_server.hpp
@@ -62,13 +62,33 @@ class HSM_RPCServiceImpl final : public HSM_RPC::Service
 };
 
 
+/**
+ * @class HSMServer
+ * @brief gRPC server that implements the HSM service and runs on the given port.
+ */
 class HSMServer {
     HSM_RPCServiceImpl service;
     std::unique_ptr<grpc::Server> server;
 
 public:
+    /**
+     * @brief Constructor
+     * @param server_address The address to bind the server to, in the format [host]:[port].
+     * @param credentials The server credentials.
+     */
     explicit HSMServer(std::string const &server_address = "0.0.0.0:50051", std::shared_ptr<grpc::ServerCredentials> credentials = grpc::InsecureServerCredentials());
+
+    /**
+     * @brief Wait until the server shuts down.
+     *
+     * \warning The server must be either shutting down or some other thread must
+     * call \a Shutdown for this function to ever return.
+     */
     void Wait();
+
+    /**
+     * @brief Stop the server.
+     */
     void shutdown();
 };
 
