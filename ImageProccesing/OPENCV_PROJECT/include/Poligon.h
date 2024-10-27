a1656b812246fd21a5bc87cc3a72faf65f8068a6
dmaro <dmaroko21@gmai.com>
2024-10-27 22:49:04 +0200

    order in video

--- ImageProccesing/OPENCV_PROJECT/include/Poligon.h
+++ ImageProccesing/OPENCV_PROJECT/include/Poligon.h
@@ -12,6 +12,8 @@ public:
 	bool doesRectIntersectPolygon(const cv::Rect& rect) const;
 	bool doesLineIntersectPolygon(const std::pair<cv::Point, cv::Point>& line) const;
 	static bool doLinesIntersect(const cv::Point& p1, const cv::Point& p2, const cv::Point& q1, const cv::Point& q2);
+
+	const std::vector<cv::Point>& getPoligon() const;
 private:
 	std::vector<cv::Point> m_poligon;
 };
--- ImageProccesing/OPENCV_PROJECT/src/Visualization.cpp
+++ ImageProccesing/OPENCV_PROJECT/src/Visualization.cpp
@@ -1,6 +1,6 @@
 #include "Visualization.h"
 #include "Poligon.h"
-
+#include "InputData.h"
 
 cv::Scalar pink = cv::Scalar(203, 192, 255);
 cv::Scalar blue = cv::Scalar(255, 0, 0);
@@ -13,20 +13,36 @@ cv::Scalar lightBlue = cv::Scalar(255, 228, 173);
 cv::Scalar purple = cv::Scalar(128, 0, 128);
 cv::Scalar orange = cv::Scalar(0, 165, 255);
 
+//=================================================================================================
+std::vector<std::pair<Poligon, ZONE_TYPES>>* Visualization::m_zonesPoligons = nullptr;
+//=================================================================================================
 
 void Visualization::drawPredictedObjects(cv::Mat& frame, std::unordered_map<int, TrackedObject>& predictedObjects)
 {
     // Draw bounding boxes for each tracked object
     for (const auto& [id, predicetedObject] : predictedObjects) {
-        const auto& bbox = predicetedObject.bboxCurr;
-        cv::Scalar color;
-        if (predicetedObject.warningPriority == WarningPriority::Safe || predicetedObject.warningPriority == WarningPriority::Low) {
-            color = zoneTypeToColor.at(predicetedObject.zoneTypeIntersect);
+        cv::Scalar color = predicetedObject.blinkInterval % 6 < 3 ?
+            warningPriorityToColor.at(predicetedObject.warningPriority) : COLORS::BLACK;
+
+        //SHADOW
+        //if (int(predicetedObject.warningPriority) <= 2)
+        //{
+        //    const cv::Rect2d& rect = predicetedObject.bboxCurr;
+        //    cv::Rect2d shadowRect(rect.x, rect.y, rect.width - 5, rect.height - 5);
+        //    cv::rectangle(frame, shadowRect, cv::Scalar(144, 144, 255), 7);
+        //}
+        
+        if (int(predicetedObject.warningPriority) <= 2)
+        {
+            cv::Mat roi = frame(predicetedObject.bboxCurr);
+
+            cv::Mat coloredOverlay;
+            roi.copyTo(coloredOverlay);
+            cv::rectangle(coloredOverlay, cv::Point(0, 0), cv::Point(predicetedObject.bboxCurr.width, predicetedObject.bboxCurr.height), cv::Scalar(0, 0, 255), -1);
+            cv::addWeighted(coloredOverlay, 0.2, roi, 1.0 - 0.2, 0, roi);
         }
-        else {
-            color = warningPriorityToColor.at(predicetedObject.warningPriority);
-        }
-        cv::rectangle(frame, bbox, color, 2);
+
+        cv::rectangle(frame, predicetedObject.bboxCurr, color, 2);
     }
 }
 
@@ -69,7 +85,7 @@ void Visualization::drawRelativeSpeedWithDistances(cv::Mat& frame, const Tracked
     std::string previousDistanceLabel = cv::format("p: %.2f m", predictedObject.distancePrev);
 
     // קביעת מיקום התצוגה על גבי הפריים
-    int baseLine = predictedObject.bboxCurr.y - 5;  // מעל המלבן
+    int baseLine = int(predictedObject.bboxCurr.y) - 5;  // מעל המלבן
     int textHeight = 15; // גובה הטקסט בין שורה לשורה
     int padding = 5; // מרווח לרקע
 
@@ -81,136 +97,103 @@ void Visualization::drawRelativeSpeedWithDistances(cv::Mat& frame, const Tracked
 
     // ציור רקעים לבנים מאחורי כל שורת טקסט
     cv::rectangle(frame,
-        cv::Point(predictedObject.bboxCurr.x - padding, baseLine - textSizeSpeed.height - padding),
-        cv::Point(predictedObject.bboxCurr.x + textSizeSpeed.width + padding, baseLine + padding),
+        cv::Point(int(predictedObject.bboxCurr.x) - padding, baseLine - textSizeSpeed.height - padding),
+        cv::Point(int(predictedObject.bboxCurr.x) + textSizeSpeed.width + padding, baseLine + padding),
         cv::Scalar(255, 255, 255), cv::FILLED);
 
     cv::rectangle(frame,
-        cv::Point(predictedObject.bboxCurr.x - padding, baseLine - textHeight - textSizeCurrent.height - padding),
-        cv::Point(predictedObject.bboxCurr.x + textSizeCurrent.width + padding, baseLine - textHeight + padding),
+        cv::Point(int(predictedObject.bboxCurr.x) - padding, baseLine - textHeight - textSizeCurrent.height - padding),
+        cv::Point(int(predictedObject.bboxCurr.x) + textSizeCurrent.width + padding, baseLine - textHeight + padding),
         cv::Scalar(255, 255, 255), cv::FILLED);
 
     cv::rectangle(frame,
-        cv::Point(predictedObject.bboxCurr.x - padding, baseLine - 2 * textHeight - textSizePrevious.height - padding),
-        cv::Point(predictedObject.bboxCurr.x + textSizePrevious.width + padding, baseLine - 2 * textHeight + padding),
+        cv::Point(int(predictedObject.bboxCurr.x) - padding, baseLine - 2 * textHeight - textSizePrevious.height - padding),
+        cv::Point(int(predictedObject.bboxCurr.x) + textSizePrevious.width + padding, baseLine - 2 * textHeight + padding),
         cv::Scalar(255, 255, 255), cv::FILLED);
 
     // הצגת מהירות יחסית בטקסט שחור
     cv::putText(frame, speedLabel,
-        cv::Point(predictedObject.bboxCurr.x, baseLine),
+        cv::Point(int(predictedObject.bboxCurr.x), baseLine),
         cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0), 2);
 
     // הצגת המרחק הנוכחי בטקסט שחור
     cv::putText(frame, currentDistanceLabel,
-        cv::Point(predictedObject.bboxCurr.x, baseLine - textHeight),
+        cv::Point(int(predictedObject.bboxCurr.x), baseLine - textHeight),
         cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0), 2);
 
     // הצגת המרחק הקודם בטקסט שחור
     cv::putText(frame, previousDistanceLabel,
-        cv::Point(predictedObject.bboxCurr.x, baseLine - 2 * textHeight),
+        cv::Point(int(predictedObject.bboxCurr.x), baseLine - 2 * textHeight),
         cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0), 2);
 }
 
 void Visualization::drawRelativeSpeed(cv::Mat& frame, const TrackedObject& predictedObject, float relativeSpeed)
 {
     std::string speedLabel = cv::format("%.2f m/s", relativeSpeed);
-    cv::putText(frame, speedLabel, cv::Point(predictedObject.bboxCurr.x, predictedObject.bboxCurr.y), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 255), 2);
+    cv::putText(frame, speedLabel, cv::Point(int(predictedObject.bboxCurr.x), int(predictedObject.bboxCurr.y)), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 255), 2);
 }
 
 void Visualization::drawCategory(cv::Mat& frame, std::unordered_map<int, TrackedObject>& predictedObjects)
 {
 }
-
-void Visualization::drawCarsHoodTarpezoid(cv::Mat& frame, std::unordered_map<int, TrackedObject>& predictedObjects)
-{
-    // מכסה מנוע של הרכב צילום:
-    std::vector<cv::Point> trapezoid_points;
-    trapezoid_points.push_back(cv::Point(0, frame.rows));   // פינה שמאלית תחתונה
-    trapezoid_points.push_back(cv::Point(frame.cols, frame.rows));  // פינה ימנית תחתונה
-    trapezoid_points.push_back(cv::Point(1277, 590));          // פינה ימנית עליונה
-    trapezoid_points.push_back(cv::Point(3, 590));          // פינה שמאלית עליונה
-
-    // צייר את הטרפז
-    const cv::Point* points[1] = { &trapezoid_points[0] };
-    int num_points = static_cast<int>(trapezoid_points.size());
-    cv::polylines(frame, points, &num_points, 1, true, cv::Scalar(255, 0, 0), 3);
-    
-    //........טרפז מספר 1:
-    // הגדר קואורדינטות הטרפז
-    trapezoid_points.clear();
-    trapezoid_points.push_back(cv::Point(111, 590));   // פינה שמאלית תחתונה
-    trapezoid_points.push_back(cv::Point(880, 590));  // פינה ימנית תחתונה
-    trapezoid_points.push_back(cv::Point(735, 450));          // פינה ימנית עליונה
-    trapezoid_points.push_back(cv::Point(390, 450));          // פינה שמאלית עליונה
-
-    // צייר את הטרפז
-    points[0] = { &trapezoid_points[0] };
-    num_points = static_cast<int>(trapezoid_points.size());
-    cv::polylines(frame, points, &num_points, 1, true, orange, 3);
-
-    //.......2רפז מספר :
-// הגדר קואורדינטות הטרפז
-    trapezoid_points.clear();
-    trapezoid_points.push_back(cv::Point(390, 450));   // פינה שמאלית תחתונה
-    trapezoid_points.push_back(cv::Point(735, 450));  // פינה ימנית תחתונה
-    trapezoid_points.push_back(cv::Point(650, 370));          // פינה ימנית עליונה
-    trapezoid_points.push_back(cv::Point(540, 370));          // פינה שמאלית עליונה
-
-    // צייר את הטרפז
-    points[0] = { &trapezoid_points[0] };
-    num_points = static_cast<int>(trapezoid_points.size());
-    cv::polylines(frame, points, &num_points, 1, true, yellow, 3);
-}
-
-
 //==================================================================
 void Visualization::drawZones(cv::Mat& frame)
 {
-    // cars hood
-    std::vector<cv::Point> trapezoid_points;
-    trapezoid_points.push_back(TRAPEZOID::CAR_HOOD::DL);// WS
-    trapezoid_points.push_back(TRAPEZOID::CAR_HOOD::DR);// ES
-    trapezoid_points.push_back(TRAPEZOID::CAR_HOOD::UR);// EN
-    trapezoid_points.push_back(TRAPEZOID::CAR_HOOD::UL);// WN
-
-    const cv::Point* points[1] = { &trapezoid_points[0] };
-    int num_points = static_cast<int>(trapezoid_points.size());
-    cv::polylines(frame, points, &num_points, 1, true, COLORS::BLACK, 3);
-
-    // CRIITICAL_ZONE_1
-    trapezoid_points.clear();
-    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_1::DL);// WS
-    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_1::DR);// ES
-    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_1::UR);// EN
-    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_1::UL);// WN
-
-    points[0] = { &trapezoid_points[0] };
-    num_points = static_cast<int>(trapezoid_points.size());
-    cv::polylines(frame, points, &num_points, 1, true, zoneTypeToColor.at(ZONE_TYPES::CRITICAL_ZONE_1), 3);
-
-    //CRITICAL_ZONE_2
-    trapezoid_points.clear();
-    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_2::DL);// WS
-    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_2::DR);// ES
-    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_2::UR);// EN
-    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_2::UL);// WN
-
-    points[0] = { &trapezoid_points[0] };
-    num_points = static_cast<int>(trapezoid_points.size());
-    cv::polylines(frame, points, &num_points, 1, true, zoneTypeToColor.at(ZONE_TYPES::CRITICAL_ZONE_2), 3);
-
-    //TRACKED_ZONE
-    trapezoid_points.clear();
-    trapezoid_points.push_back(TRAPEZOID::TRACKED_ZONE::DL);// WS
-    trapezoid_points.push_back(TRAPEZOID::TRACKED_ZONE::DR);// ES
-    trapezoid_points.push_back(TRAPEZOID::TRACKED_ZONE::UR);// EN
-    trapezoid_points.push_back(TRAPEZOID::TRACKED_ZONE::UL);// WN
-
-    points[0] = { &trapezoid_points[0] };
-    num_points = static_cast<int>(trapezoid_points.size());
-    cv::polylines(frame, points, &num_points, 1, true, zoneTypeToColor.at(ZONE_TYPES::TRACKED_ZONE), 3);
+    for (auto& [poligon, zoneType] : *m_zonesPoligons)
+    {
+        const std::vector<cv::Point>& poligonVec = poligon.getPoligon();
+        const cv::Point* points[1] = { &poligonVec[0]};
+        int num_points = static_cast<int>(poligonVec.size());
+        cv::polylines(frame, points, &num_points, 1, true, zoneTypeToColor.at(zoneType), 3);
+    }
 }
 
+//void Visualization::drawZones(cv::Mat& frame)
+//{
+//    // cars hood
+//    std::vector<cv::Point> trapezoid_points;
+//    trapezoid_points.push_back(TRAPEZOID::CAR_HOOD::DL);// WS
+//    trapezoid_points.push_back(TRAPEZOID::CAR_HOOD::DR);// ES
+//    trapezoid_points.push_back(TRAPEZOID::CAR_HOOD::UR);// EN
+//    trapezoid_points.push_back(TRAPEZOID::CAR_HOOD::UL);// WN
+//
+//    const cv::Point* points[1] = { &trapezoid_points[0] };
+//    int num_points = static_cast<int>(trapezoid_points.size());
+//    cv::polylines(frame, points, &num_points, 1, true, COLORS::BLACK, 3);
+//
+//    // CRIITICAL_ZONE_1
+//    trapezoid_points.clear();
+//    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_1::DL);// WS
+//    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_1::DR);// ES
+//    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_1::UR);// EN
+//    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_1::UL);// WN
+//
+//    points[0] = { &trapezoid_points[0] };
+//    num_points = static_cast<int>(trapezoid_points.size());
+//    cv::polylines(frame, points, &num_points, 1, true, zoneTypeToColor.at(ZONE_TYPES::CRITICAL_ZONE_1), 3);
+//
+//    //CRITICAL_ZONE_2
+//    trapezoid_points.clear();
+//    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_2::DL);// WS
+//    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_2::DR);// ES
+//    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_2::UR);// EN
+//    trapezoid_points.push_back(TRAPEZOID::CRITICAL_ZONE_2::UL);// WN
+//
+//    points[0] = { &trapezoid_points[0] };
+//    num_points = static_cast<int>(trapezoid_points.size());
+//    cv::polylines(frame, points, &num_points, 1, true, zoneTypeToColor.at(ZONE_TYPES::CRITICAL_ZONE_2), 3);
+//
+//    //TRACKED_ZONE
+//    trapezoid_points.clear();
+//    trapezoid_points.push_back(TRAPEZOID::TRACKED_ZONE::DL);// WS
+//    trapezoid_points.push_back(TRAPEZOID::TRACKED_ZONE::DR);// ES
+//    trapezoid_points.push_back(TRAPEZOID::TRACKED_ZONE::UR);// EN
+//    trapezoid_points.push_back(TRAPEZOID::TRACKED_ZONE::UL);// WN
+//
+//    points[0] = { &trapezoid_points[0] };
+//    num_points = static_cast<int>(trapezoid_points.size());
+//    cv::polylines(frame, points, &num_points, 1, true, zoneTypeToColor.at(ZONE_TYPES::TRACKED_ZONE), 3);
+//}
 void Visualization::drawWarningDescription(cv::Mat& frame, const TrackedObject& trackedObject)
 {
 	int fontFace = cv::FONT_HERSHEY_SIMPLEX;
@@ -220,11 +203,16 @@ void Visualization::drawWarningDescription(cv::Mat& frame, const TrackedObject& 
 	int baseline = 0;
 	cv::Size textSize = cv::getTextSize(trackedObject.warningDescription, fontFace, fontScale, thickness, &baseline);
 
-	cv::Point textOrg(trackedObject.bboxCurr.x, trackedObject.bboxCurr.y);  // המיקום של הטקסט
+	cv::Point textOrg(int(trackedObject.bboxCurr.x), int(trackedObject.bboxCurr.y));  // המיקום של הטקסט
 
 	cv::Point rectStart(textOrg.x, textOrg.y - textSize.height);
 	cv::Point rectEnd(textOrg.x + textSize.width, textOrg.y);
 
 	cv::rectangle(frame, rectStart, rectEnd, cv::Scalar(255, 255, 255), cv::FILLED);
 	cv::putText(frame, trackedObject.warningDescription, textOrg, fontFace, fontScale, cv::Scalar(0, 0, 0), thickness);
+}
+
+void Visualization::setCurrentVideoPoligons(const std::string& videoName)
+{
+    m_zonesPoligons = &(InputData::getInstance().getZonesPoligon(videoName));
 }
